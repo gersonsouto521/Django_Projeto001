5cf1c6a046d1dc190fa589362c1924cf9c3a1f3b
gersonsouto <gersonsouto94@gmail.com>
2019-12-23 00:23:03 -0300

    puxando tarefas do banco

--- To_Do/task/views.py
+++ To_Do/task/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Task
+
+def taskView(request, id):
+    task = get_object_or_404(Task, pk=id)
+    return render(request, 'task/task.html', {'task':task})
+    
+def taskList (request):
+    tasks = Task.objects.all()
+    return render(request, 'task/list.html', {'tasks': tasks})
 
 def helloWorld (request):
     return HttpResponse('Hello World!!!')
-
-def taskList (request):
-    return render(request, 'task/list.html')
 
 def yourName(request, name):
     return render(request, 'task/yourname.html',{'name':name})
