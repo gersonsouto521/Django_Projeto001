15f2af25f65bfabba563f83e880cbd0169c53eab
gersonsouto <gersonsouto94@gmail.com>
2019-12-26 22:37:45 -0300

    botao delete tasks

--- To_Do/task/views.py
+++ To_Do/task/views.py
@@ -32,6 +32,11 @@ def editTask(request, id):
     else:
         return render(request, 'task/edittask.html', {'form':form, 'task':task})
 
+def deleteTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    task.delete()
+    return redirect('/')
+
 def taskView(request, id):
     task = get_object_or_404(Task, pk=id)
     return render(request, 'task/task.html', {'task':task})
