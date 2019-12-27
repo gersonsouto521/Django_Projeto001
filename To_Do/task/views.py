07e4f8821b10f43aac864c4c227c95ab05462c47
gersonsouto <gersonsouto94@gmail.com>
2019-12-26 22:08:40 -0300

    botao editar task

--- To_Do/task/views.py
+++ To_Do/task/views.py
@@ -17,6 +17,21 @@ def newTask(request):
         form = TaskForm()
         return render(request,'task/addtask.html', {'form':form})
 
+def editTask(request, id):
+    task = get_object_or_404(Task, pk =id)
+    form = TaskForm(instance=task)
+
+    if (request.method == 'POST'):
+        form = TaskForm(request.POST, instance=task)
+
+        if(form.is_valid()):
+            task.save()
+            return redirect('/')
+        else:
+            return render(request, 'task/edittask.html', {'form':form, 'task':task})
+    else:
+        return render(request, 'task/edittask.html', {'form':form, 'task':task})
+
 def taskView(request, id):
     task = get_object_or_404(Task, pk=id)
     return render(request, 'task/task.html', {'task':task})
