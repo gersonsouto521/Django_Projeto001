6a0cbaae0fc210b972807aa8c08777d831642cdd
gersonsouto <gersonsouto94@gmail.com>
2020-01-21 23:30:22 -0300

    filtro tarefas done and doing

--- To_Do/task/static/js/scripts.js
+++ To_Do/task/static/js/scripts.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
 
+    var baseUrl = 'http://127.0.0.1:8000/';
     var deleteBtn = $('.delete-btn');
     var searchBtn = $('#search-btn');
     var searchForm = $('#search-form');
@@ -19,6 +20,11 @@ $( document ).ready(function() {
 
     $(searchBtn).on('click', function(){
         searchForm.submit();
+    }); 
+     
+    $(filter).change(function(){
+        var filter = $(this).val();
+        window.location.href = baseUrl + '?filter=' + filter;
     });
 
 
